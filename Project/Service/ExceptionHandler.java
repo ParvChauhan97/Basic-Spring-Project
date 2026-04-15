e0678b6e427f1de1b9db9e032415d453cc139a52
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-15 14:30:51 +0530

    Update ExceptionHandler.java

--- Project/Service/ExceptionHandler.java
+++ Project/Service/ExceptionHandler.java
@@ -6,3 +6,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+
+@RestControllerAdvice
+public class ExceptionHandler {
+
+   @org.springframework.web.bind.annotation.ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<Map<String, Object>> ResponseException(Exception exception) {
+        Map<String, Object> exp = new HashMap<>();
+        exp.put("TimeStamp", LocalDateTime.now());
+        exp.put("Status", HttpStatus.BAD_REQUEST);
+        exp.put("Error", "Error");
+        exp.put("Message", exception.getMessage());
+        return new ResponseEntity<>(exp, HttpStatus.BAD_REQUEST);
+    }
+}
